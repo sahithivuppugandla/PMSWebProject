1b385ef2dceecbee4df23af82821a48c07073912
Sahithi <sahithi.v403@gmail.com>
2020-11-08 14:18:24 +0530

    new

--- LoanApp/WebContent/footer.jsp
+++
@@ -1,1 +0,0 @@
-<h5 align="center" style="color:green">A statutory body registered under RBI</h5>
--- LoanApp/src/com/wellsfargo/batch5/pms/exception/LoanException.java
+++
@@ -1,7 +0,0 @@
-package com.wellsfargo.batch5.pms.exception;
-
-public class LoanException extends Exception{
-	public LoanException(String errMsg) {
-		super(errMsg);
-	}
-}
